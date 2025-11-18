bfc8dff0b1baef725ecfe2f104aa44584ed4f4fd
burntchrispy <113560799+chndb@users.noreply.github.com>
2025-11-18 13:58:16 -0500

    Update gamescreen.js

--- app/gamescreen.js
+++ app/gamescreen.js
@@ -1,3 +1,22 @@
+//Scale x to window, fraction input only
+function percX(x) {
+    return Math.floor(x * WIDTH);
+}
+
+//Scale y to window, fraction input only
+function percY(y) {
+    return Math.floor(y * HEIGHT);
+}
+
+grimBGLoaded = false;
+let grimBG;
 function drawGame() {
-  
+  if (!grimBGLoaded) {
+        grimBG = new Image();
+        grimBG.src = 'assets/grid.png';
+        grimBGLoaded = true;
+    }
+    if (grimBG.complete) {
+        context.drawImage(grimBG, percX(0), percY(0));
+    }
 }
